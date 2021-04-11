99799a5d0c1f08d976ac770bbae20e8ffa7ba0d7
Tes3awy <osaid2512@gmail.com>
2021-04-11 20:46:27 +0200

    Add all status code reply cases

--- cisco_dna_lab.py
+++ cisco_dna_lab.py
@@ -192,6 +192,7 @@ if r.status_code == 200:
             try:
                 workbook.close()
                 print(f"✔ {workbook_title} file is created successfully!")
+                print(f"✔ Opening {workbook_title} ... please wait. ✔")
                 time.sleep(1)
                 webbrowser.open(os.path.join(workbook_title))
             except xlsxwriter.exceptions.FileCreateError as e:
@@ -202,16 +203,40 @@ if r.status_code == 200:
             break
     elif r.status_code == 204:
         print("❕ The request was successful, however no content was returned.")
-    else:
+    elif r.status_code == 206:
         print(
-            "❌ Something went wrong! The client made a request for a resource that does not exist."
+            "❕ The GET request included a Range Header, and the server responded with the partial content matching the range."
         )
+    elif r.status_code == 400:
+        print("❌ The client made a request that the server could not understand.")
+    elif r.status_code == 401:
+        print(
+            "❌ The client's authentication credentials included with the request are missing or invalid."
+        )
+    elif r.status_code == 403:
+        print(
+            "❕ The server recognizes the authentication credentials, but the client is not authorized to perform this request."
+        )
+    elif r.status_code == 404:
+        print(
+            "❕ The client made a request for a resource that does not exist. Please check the URLs."
+        )
+    elif r.status_code == 409:
+        print("❌ The target resource is in a conflicted state.")
+    elif r.status_code == 415:
+        print(
+            "❌ The client sent a request body in a format that the server does not support."
+        )
+    elif r.status_code == 500:
+        print("❌ The server could not fulfill the request.")
+    elif r.status_code == 501:
+        print(
+            "❌ The server has not implemented the functionality required to fulfill the request."
+        )
+    elif r.status_code == 503:
+        print("❕ The server is (temporarily) unavailable.")
+    else:
+        print("❕ The server did not respond inside time restrictions and timed-out.")
 
-elif r.status_code == 404:
-    print("❕ Please check the URLs")
-elif r.status_code == 401:
-    print("❌ Invalid Credentials")
 else:
-    print(
-        "❌ Oops! Something went wrong. Can't get an Auth Token now. Please try again later."
-    )
+    print("❌ Invalid Credentials.")
