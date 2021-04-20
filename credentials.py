36be7877faf5beacdca304450f3a2c1023e82aa3
Tes3awy <osaid2512@gmail.com>
2021-04-20 23:54:29 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # Cisco DNA Center AO 2.1.2.5
-base_url = "https://sandboxdnac2.cisco.com"
-username = "devnetuser"
-password = "Cisco123!"
-ssl_certificate = True
+BASE_URL = "https://sandboxdnac2.cisco.com"
+USERNAME = "devnetuser"
+PASSWORD = "Cisco123!"
+SSL_CERTIFICATE = True
